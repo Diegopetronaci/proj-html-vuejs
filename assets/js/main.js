6c1c7c983feccd8ba998a10a6cf9e86106947aec
Diego <Petronacidiego@gmail.com>
2021-01-20 11:36:41 +0100

    push

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        linksNav: [
+            "HOME",
+            "ABOUT",
+            "PRICES",
+            "COURSES",
+            "LOCATIONS",
+            "BLOG",
+        ],
+    }
+})
