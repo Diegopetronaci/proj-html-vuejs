f36e07604f0ded6580b33eeef5222c99f1a2464a
Diego <Petronacidiego@gmail.com>
2021-01-21 15:23:39 +0100

    estetica di base completata

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,51 @@ let app = new Vue({
             "LOCATIONS",
             "BLOG",
         ],
+
+        infoFooter: [
+            {
+                icona: "fas fa-home",
+                text: "12345 North Main Street. New york, NY 555555"
+            },
+            {
+                icona: "fas fa-phone",
+                text: "1.800.555.6789"
+            },
+            {
+                icona: "far fa-envelope",
+                text: "info@your-domain.com"
+            },
+            {
+                icona: "fas fa-link",
+                text: "Theme-Fusion.com"
+            },
+        ],
+
+        elementFooter: [
+            {
+                icona: "far fa-arrow-alt-circle-right",
+                text: "Pass Plus"
+            },
+            {
+                icona: "far fa-arrow-alt-circle-right",
+                text: "Intensive Course"
+            },
+            {
+                icona: "far fa-arrow-alt-circle-right",
+                text: "Automatic"
+            },
+            {
+                icona: "far fa-arrow-alt-circle-right",
+                text: "Instructor Training"
+            },
+        ]
+    },
+    methods: {
+        shopOffer(){
+            alert("Grazie per aver scelto Driving School !!")
+        },
+        mandaAiVideos(){
+            alert("Al momento non è ancora presente una pagina Video, riprova quando sarò un programmatore migliore!!")
+        }
     }
 })
